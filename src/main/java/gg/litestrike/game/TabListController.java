7e4c5ce0111a4055ef5898e84c97bcbb0d9dc477
cooltexture <cooltexture@bits-mampfer.eu>
2024-12-05 23:42:46 +0100

    it shows, but has to be aligned still

--- src/main/java/gg/litestrike/game/TabListController.java
+++ src/main/java/gg/litestrike/game/TabListController.java
@@ -30,9 +30,8 @@ class TabListController {
 							.append(render_player_stat(p))
 							.append(text("\n---------------------------------------------------\n\n").color(NamedTextColor.GRAY)));
 
-					Component title = text("LITESTRIKE").color(NamedTextColor.GREEN).decoration(TextDecoration.BOLD, true)
-							.append(text(" \uE100").color(NamedTextColor.WHITE));
-					p.sendPlayerListHeader(title);
+					p.sendPlayerListHeader(text("LITESTRIKE").color(NamedTextColor.GREEN).decoration(TextDecoration.BOLD, true)
+							.append(text(" \uE100").color(NamedTextColor.WHITE)));
 				}
 
 			}
@@ -51,7 +50,6 @@ class TabListController {
 
 		for (PlayerData pd : gc.playerDatas) {
 			Player player = Bukkit.getPlayer(pd.player);
-			Component player_status = text("\n ");
 			Component player_stats = text(" \uE101 ") // sword death icon
 					.append(text(pd.kills))
 					.append(text(" \uE103 ")) // Death icon
@@ -60,24 +58,25 @@ class TabListController {
 					.append(text(pd.calc_player_score()));
 
 			if (player == null) {
-				player_status.append(text("[Disconnected] "));
-				player_status.append(text(pd.player).color(NamedTextColor.GRAY));
-				player_status.append(player_stats);
-				disc_list.add(player_status);
+				disc_list.add(text("\n ")
+						.append(text("[Disconnected] "))
+						.append(text(pd.player).color(NamedTextColor.GRAY))
+						.append(player_stats));
 				continue;
 			}
 
+			Component player_status = text("\n ");
 			if (player.getGameMode() == GameMode.SPECTATOR) {
-				player_status.append(text("[Dead] "));
+				player_status = player_status.append(text("[Dead] "));
 			} else {
-				player_status.append(text("[Alive] "));
+				player_status = player_status.append(text("[Alive] "));
 			}
 			if (gc.teams.get_team(player) == Team.Placer) {
-				player_status.append(text(pd.player).color(Teams.PLACER_RED));
+				player_status = player_status.append(text(pd.player).color(Teams.PLACER_RED));
 			} else {
-				player_status.append(text(pd.player).color(Teams.BREAKER_GREEN));
+				player_status = player_status.append(text(pd.player).color(Teams.BREAKER_GREEN));
 			}
-			player_status.append(player_stats);
+			player_status = player_status.append(player_stats);
 			if (gc.teams.get_team(player) == p_team) {
 				allay.add(player_status);
 			} else {
@@ -86,13 +85,13 @@ class TabListController {
 		}
 
 		for (Component c : allay) {
-			footer.append(c);
+			footer = footer.append(c);
 		}
 		for (Component c : enemy) {
-			footer.append(c);
+			footer = footer.append(c);
 		}
 		for (Component c : disc_list) {
-			footer.append(c);
+			footer = footer.append(c);
 		}
 
 		return footer;
