9a0944d54b80355d5d98f5fd9bd682381575b768
TotallyNoCallum <Callumhargreaves2009@outlook.com>
2025-01-06 21:48:37 +0000

    Bedrock support

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,13 +8,15 @@ version = "1.0-SNAPSHOT"
 repositories {
     mavenCentral()
     maven("https://repo.papermc.io/repository/maven-public/")
-		maven("https://repo.dmulloy2.net/repository/public/")
+	maven("https://repo.dmulloy2.net/repository/public/")
+    maven("https://repo.opencollab.dev/main/")
 }
 
 dependencies {
     compileOnly("io.papermc.paper:paper-api:1.21.4-R0.1-SNAPSHOT")
     implementation("org.xerial:sqlite-jdbc:3.47.0.0")
     compileOnly("com.comphenix.protocol:ProtocolLib:5.3.0")
+    compileOnly("org.geysermc.floodgate:api:2.2.3-SNAPSHOT")
 }
 
 java {
