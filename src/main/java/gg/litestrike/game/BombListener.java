fd776da93041128d3145500e00059676d81d7696
cooltexture <cooltexture@bits-mampfer.eu>
2025-03-15 23:55:16 +0100

    made gapples droppable prevent bow from not working when clicking interactable blocks

--- src/main/java/gg/litestrike/game/BombListener.java
+++ src/main/java/gg/litestrike/game/BombListener.java
@@ -247,7 +247,7 @@ public class BombListener implements Listener {
 
 	@EventHandler(priority = EventPriority.LOW)
 	public void onInteractPlacing(PlayerInteractEvent e) {
-		if (e.getClickedBlock() != null && e.getClickedBlock().getType().isInteractable()) {
+		if (e.getClickedBlock() != null && isInteractable(e.getClickedBlock().getType())) {
 			e.setCancelled(true);
 		}
 
@@ -315,10 +315,14 @@ public class BombListener implements Listener {
 				e.setCancelled(true);
 			}
 			String item_name = e.getItemDrop().getItemStack().getType().toString().toLowerCase();
-			if (!(item_name.contains("arrow") || e.getItemDrop().getItemStack().getType() == Material.POTION)) {
+			if (item_name.contains("arrow")
+					|| e.getItemDrop().getItemStack().getType() == Material.POTION
+					|| e.getItemDrop().getItemStack().getType() == Material.GOLDEN_APPLE) {
+				return;
+			} else {
 				e.setCancelled(true);
+				return;
 			}
-			return;
 		}
 		reset();
 		InvItemBomb ib = (InvItemBomb) gc.bomb;
@@ -403,6 +407,71 @@ public class BombListener implements Listener {
 
 	private int ping_compensation_ticks(Player p) {
 		return Math.min(p.getPing() / 50, 10);
+	}
+
+	// i got this from here:
+	// https://www.spigotmc.org/threads/check-if-a-block-is-interactable.535861/
+	public static boolean isInteractable(Material type) {
+		boolean interactable = type.isInteractable();
+		if (!interactable)
+			return false;
+
+		switch (type) {
+			case ACACIA_STAIRS:
+			case ANDESITE_STAIRS:
+			case BIRCH_STAIRS:
+			case BLACKSTONE_STAIRS:
+			case BRICK_STAIRS:
+			case COBBLESTONE_STAIRS:
+			case CRIMSON_STAIRS:
+			case DARK_OAK_STAIRS:
+			case DARK_PRISMARINE_STAIRS:
+			case DIORITE_STAIRS:
+			case END_STONE_BRICK_STAIRS:
+			case GRANITE_STAIRS:
+			case JUNGLE_STAIRS:
+			case MOSSY_COBBLESTONE_STAIRS:
+			case MOSSY_STONE_BRICK_STAIRS:
+			case NETHER_BRICK_STAIRS:
+			case OAK_STAIRS:
+			case POLISHED_ANDESITE_STAIRS:
+			case POLISHED_BLACKSTONE_BRICK_STAIRS:
+			case POLISHED_BLACKSTONE_STAIRS:
+			case POLISHED_DIORITE_STAIRS:
+			case POLISHED_GRANITE_STAIRS:
+			case PRISMARINE_BRICK_STAIRS:
+			case PRISMARINE_STAIRS:
+			case PURPUR_STAIRS:
+			case QUARTZ_STAIRS:
+			case RED_NETHER_BRICK_STAIRS:
+			case RED_SANDSTONE_STAIRS:
+			case SANDSTONE_STAIRS:
+			case SMOOTH_QUARTZ_STAIRS:
+			case SMOOTH_RED_SANDSTONE_STAIRS:
+			case SMOOTH_SANDSTONE_STAIRS:
+			case SPRUCE_STAIRS:
+			case STONE_BRICK_STAIRS:
+			case STONE_STAIRS:
+			case WARPED_STAIRS:
+
+			case ACACIA_FENCE:
+			case BIRCH_FENCE:
+			case CRIMSON_FENCE:
+			case DARK_OAK_FENCE:
+			case JUNGLE_FENCE:
+			case MOVING_PISTON:
+			case NETHER_BRICK_FENCE:
+			case OAK_FENCE:
+			case PUMPKIN:
+			case REDSTONE_ORE:
+			case REDSTONE_WIRE:
+			case SPRUCE_FENCE:
+			case WARPED_FENCE:
+				return false;
+			default:
+				return true;
+		}
+
 	}
 }
 
