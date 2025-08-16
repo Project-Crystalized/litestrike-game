1698c5a422aa64c735655373185b5774c9125a81
cooltexture <cooltexture@bits-mampfer.eu>
2025-08-16 14:44:40 +0200

    add new database script for a specific player

--- database_scripts/print_specific_game.py
+++ database_scripts/print_specific_game.py
@@ -5,6 +5,7 @@ import uuid
 import requests
 import sys
 from datetime import datetime
+from pathlib import Path
 
 if len(sys.argv) < 2:
     print("error! no game reference given")
@@ -15,7 +16,7 @@ if len(sys.argv) < 2:
 game_ref = int(sys.argv[1])
 print("printing out information on game with reference: ", game_ref)
 
-con = sqlite3.connect("/home/nico/databases/litestrike_db.sql")
+con = sqlite3.connect(Path.home() / "databases/litestrike_db.sql")
 cur = con.cursor()
 res = cur.execute("SELECT * FROM LitestrikeGames WHERE game_ref = ?;", (game_ref,));
 
@@ -46,7 +47,6 @@ for row in res.fetchall():
 
     uuid_player = uuid.UUID(bytes=uuid_bytes)
     try:
-        print(f"https://sessionserver.mojang.com/session/minecraft/profile/{uuid_player}")
         data = requests.get(f"https://sessionserver.mojang.com/session/minecraft/profile/{uuid_player}").json()
         print("stats of player ", data["name"], f" ({uuid_player}) ")
     except requests.JSONDecodeError as e:
@@ -60,9 +60,9 @@ for row in res.fetchall():
     print(f"They spent {spent_money} money")
     print(f"They dealt {damage_dealt} damage")
     if was_winner:
-        print("They where part of the winning team")
+        print("They were part of the winning team")
     else:
-        print("They where part of the loosing team")
+        print("They were part of the loosing team")
     total_damage += damage_dealt if damage_dealt else 0
     total_kills += kills
     total_assists += assists
@@ -74,7 +74,7 @@ number_of_rounds = placer_wins + breaker_wins
 print()
 print()
 
-print(f"total money spent: {total_money_spent}$ which is {total_money_spent/number_of_rounds} $/round, or {total_money_spent/number_of_player} $/player on avarage")
-print(f"total assists: {total_assists} which is {total_assists/number_of_rounds} assists/round, or {total_assists/number_of_player} assists/player")
-print(f"total kills: {total_kills} which is {total_kills/number_of_rounds} kills/round, or {total_kills/number_of_player} kills/player")
-print(f"total damage dealt {total_damage} which is {total_damage/number_of_rounds} damage/round, or {total_damage/number_of_player} damage/player")
+print(f"total money spent: {total_money_spent}$ which is {total_money_spent/number_of_rounds:.2f} $/round, or {total_money_spent/number_of_player:.2f} $/player on avarage")
+print(f"total assists: {total_assists} which is {total_assists/number_of_rounds:.2f} assists/round, or {total_assists/number_of_player:.2f} assists/player")
+print(f"total kills: {total_kills} which is {total_kills/number_of_rounds:.2f} kills/round, or {total_kills/number_of_player:.2f} kills/player")
+print(f"total damage dealt {total_damage} which is {total_damage/number_of_rounds:.2f} damage/round, or {total_damage/number_of_player:.2f} damage/player")
