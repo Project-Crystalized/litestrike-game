1746d212b36397a61c75f9faee5b162d68bf79e4
cooltexture <cooltexture@bits-mampfer.eu>
2025-08-28 23:25:06 +0200

    added a new way for jumppads to work, for testing also reordered some code in MapData cause the layout was confusing

--- src/main/java/gg/litestrike/game/mapfeatures/Pads.java
+++ src/main/java/gg/litestrike/game/mapfeatures/Pads.java
@@ -1,6 +1,7 @@
 package gg.litestrike.game.mapfeatures;
 
 import org.bukkit.GameMode;
+import org.bukkit.Material;
 import org.bukkit.block.Block;
 import org.bukkit.block.BlockFace;
 import org.bukkit.entity.Player;
@@ -9,6 +10,10 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerMoveEvent;
 import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
+import org.bukkit.util.Vector;
+
+import com.destroystokyo.paper.event.player.PlayerJumpEvent;
+
 import net.kyori.adventure.key.Key;
 import net.kyori.adventure.sound.Sound;
 
@@ -48,6 +53,20 @@ class LeviPadListener implements Listener {
 }
 
 class JumpPadListener implements Listener {
+	// TODO remove this, it was testing a different way of jump pads
+	@EventHandler
+	public void onJump(PlayerJumpEvent e) {
+		Player p = e.getPlayer();
+		if (p.getGameMode() == GameMode.SPECTATOR)
+			return;
+		Block block_under = p.getLocation().getBlock().getRelative(BlockFace.DOWN);
+		if (block_under.getType() == Material.BLUE_CONCRETE_POWDER) {
+			p.setVelocity(p.getVelocity().add(new Vector(0, 1.3, 0)));
+			p.playSound(Sound.sound(Key.key("crystalized:effect.hazard_positive"), Sound.Source.AMBIENT, 1f, 1f));
+			MapFeatures.fall_protect_player(p, (20 * 4));
+		}
+	}
+
 	@EventHandler
 	public void onPlayerMove(PlayerMoveEvent e) {
 		Player p = e.getPlayer();
