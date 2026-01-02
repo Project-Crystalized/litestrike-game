546b7f5c2c0f969c67cb2bf449faee6926e6d8a9
cooltexture <cooltexture@bits-mampfer.eu>
2026-01-03 00:43:15 +0100

    chenged cargo door to use coordinates of new map

--- src/main/java/gg/litestrike/game/mapfeatures/CargoDoor.java
+++ src/main/java/gg/litestrike/game/mapfeatures/CargoDoor.java
@@ -16,8 +16,10 @@ import gg.litestrike.game.Litestrike;
 import net.kyori.adventure.key.Key;
 import net.kyori.adventure.sound.Sound;
 
+
 public class CargoDoor implements Listener {
 	private boolean is_door_open = false;
+	final int DOOR_Y = 23;
 
 	@EventHandler
 	public void onInteractLever(PlayerInteractEvent e) {
@@ -38,16 +40,16 @@ public class CargoDoor implements Listener {
 				i++;
 				switch (i) {
 					case 1:
-						door_animation(-2);
+						door_animation(-54);
 						break;
 					case 15:
-						door_animation(2);
+						door_animation(-50);
 						break;
 					case 30:
-						door_animation(6);
+						door_animation(-46);
 						break;
 					case 45:
-						door_animation(10);
+						door_animation(-42);
 						cancel();
 						break;
 				}
@@ -57,23 +59,23 @@ public class CargoDoor implements Listener {
 
 	public void close_door() {
 		is_door_open = false;
-		door_animation(-2);
+		door_animation(-54);
 	}
 
 	private void door_animation(int y) {
 		Random ran = new Random();
 		World w = Bukkit.getWorld("world");
 		Sound sound = Sound.sound(Key.key("minecraft:block.iron_door.close"), Sound.Source.AMBIENT, 3f, 0.5f);
-		Bukkit.getServer().playSound(sound, -39, 56, 50);
-		for (int x = -50; x < -32; x++) {
-			for (int z = 45; z < 55; z++) {
+		Bukkit.getServer().playSound(sound, 182, DOOR_Y, 157);
+		for (int x = 176; x < 187; x++) {
+			for (int z = 152; z < 170; z++) {
 				// copy block
-				Block b = w.getBlockAt(x, 56, z);
+				Block b = w.getBlockAt(x, DOOR_Y, z);
 				b.setType(w.getBlockAt(x, y, z).getType());
 
 				// set particle
 				if (ran.nextInt(3) == 2) { // 33% chance
-					w.spawnParticle(Particle.LARGE_SMOKE, x, 56, z, 20);
+					w.spawnParticle(Particle.LARGE_SMOKE, x, DOOR_Y, z, 20);
 				}
 			}
 		}
