7b280ab3f39546986577bece9c558258d6681f6a
cooltexture <cooltexture@bits-mampfer.eu>
2026-09-12 15:01:48 +0200

    Revert "added translation keys to tablist"
    This reverts commit e2cd01d38e7b2e9d36aea9b5315ad41bbab66ac8.

--- src/main/java/gg/litestrike/game/TabListController.java
+++ src/main/java/gg/litestrike/game/TabListController.java
@@ -110,20 +110,20 @@ class TabListController {
 		String tag;
 		TextColor name_color;
 		if (player == null) {
-			tag = "crystalized.game.generic.disconnected";
+			tag = "[Disconnected] ";
 			name_color = NamedTextColor.GRAY;
 		} else if (player.getGameMode() == GameMode.SPECTATOR) {
-			tag = "crystalized.game.generic.dead";
+			tag = "[Dead] ";
 			name_color = NamedTextColor.GRAY;
 		} else if (gc.teams.get_team(player) == Team.Placer) {
-			tag = "crystalized.game.generic.alive";
+			tag = "[Alive] ";
 			name_color = Teams.PLACER_RED;
 		} else {
-			tag = "crystalized.game.generic.alive";
+			tag = "[Alive] ";
 			name_color = Teams.BREAKER_GREEN;
 		}
 
-		Component status = text("\n ").append(translatable(tag));
+		Component status = text("\n ").append(text(tag));
 		if (rank != null) {
 			status = status.append(rank);
 		}
