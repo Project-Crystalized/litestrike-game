7ed41ba908f939ffbeaffb5903ae9db777f16616
ladycattv <ladyccattv@gmail.com>
2026-09-17 17:12:09 +0200

    registered packetevents listener

--- src/main/java/gg/litestrike/game/Litestrike.java
+++ src/main/java/gg/litestrike/game/Litestrike.java
@@ -1,6 +1,8 @@
 package gg.litestrike.game;
 
 import com.github.retrooper.packetevents.PacketEvents;
+import com.github.retrooper.packetevents.event.EventManager;
+import com.github.retrooper.packetevents.event.PacketListenerPriority;
 import com.google.common.io.ByteArrayDataInput;
 import com.google.common.io.ByteArrayDataOutput;
 import com.google.common.io.ByteStreams;
@@ -60,6 +62,8 @@ public final class Litestrike extends JavaPlugin implements PluginMessageListene
 		PacketEvents.setAPI(SpigotPacketEventsBuilder.build(this));
 		PacketEvents.getAPI().getSettings().reEncodeByDefault(false).checkForUpdates(true).bStats(false);
 		PacketEvents.getAPI().load();
+		EventManager events = PacketEvents.getAPI().getEventManager();
+		events.registerListener(new ProtocolLibLib(), PacketListenerPriority.NORMAL);
 	}
 
 	@Override
