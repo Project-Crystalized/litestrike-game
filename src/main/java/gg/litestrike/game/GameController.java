05edb71050056f7cfefa564e3dfb52c6fb586b39
ladycattv <ladyccattv@gmail.com>
2026-09-24 18:27:16 +0200

    tweaks to spec

--- src/main/java/gg/litestrike/game/GameController.java
+++ src/main/java/gg/litestrike/game/GameController.java
@@ -356,6 +356,7 @@ public class GameController {
 
 		try {
 			for (Player p : Bukkit.getOnlinePlayers()) {
+				p.setGameMode(GameMode.ADVENTURE);
 				InventoryManager.giveLobbyItems(p);
 				p.getInventory().setItem(App.BackToHub.slot, App.BackToHub.build());
 				p.getInventory().setItem(App.Requeue.slot, App.Requeue.build());
--- src/main/java/gg/litestrike/game/PlayerListener.java
+++ src/main/java/gg/litestrike/game/PlayerListener.java
@@ -122,7 +122,7 @@ public class PlayerListener implements Listener {
 
 		} else {
 			// if we are here, it means the player is rejoining
-			GameController.setSpectator(p);
+			p.setGameMode(GameMode.SPECTATOR);
 			Shop s = gc.getShop(p);
 
 			if (s != null) {
